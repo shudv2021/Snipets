5bedb1239f6cb6a2924e151472d15337f23b5c30
dmitry <shudv@yandex.ru>
2022-07-04 13:46:49 +0300

    chenge dictionary snipet

--- word_counter.rb
+++ word_counter.rb
@@ -1,10 +1,19 @@
-
+require 'pry'
 f = File.open('word_counter.txt', 'r')
+f_output = File.open('result_file.txt', 'a')
 
 f.each_line do |line|
-  
-  line.gsub!(/\s|\n|,|\.|!/)
-  puts line.split(/ /)
+  arr_cleaner = [',', '.', ';', '\'', '!']
+  arr_cleaner.each do |clearn_simbol|
+  line.gsub!(clearn_simbol, '')
+  end
+  line.gsub!(/\s+/, ' ')
+  arr = line.split(/\s/)
+    arr.each do |word|
+    f_output.puts(word)
+    end
+    
 end
 
 f.close
+f_output.close
