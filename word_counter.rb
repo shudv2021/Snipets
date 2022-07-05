a1438c07a75b74e56689166e81ba23dee7fa27f8
dmitry <shudv@yandex.ru>
2022-07-05 12:17:56 +0300

    improved word_counter

--- word_counter.rb
+++ word_counter.rb
@@ -1,19 +1,31 @@
 require 'pry'
+
+ARR_CLEANER = [',', '.', ';', '\'', '!', '“', '”' '(', ')', '?', ' to ', ' a ', ' is ', ' of ', ' the ']
+words_hash = {}
+
 f = File.open('word_counter.txt', 'r')
-f_output = File.open('result_file.txt', 'a')
+#f_output = File.open('result_file.txt', 'a')
 
 f.each_line do |line|
-  arr_cleaner = [',', '.', ';', '\'', '!']
-  arr_cleaner.each do |clearn_simbol|
+  
+  ARR_CLEANER.each do |clearn_simbol|
   line.gsub!(clearn_simbol, '')
   end
   line.gsub!(/\s+/, ' ')
   arr = line.split(/\s/)
     arr.each do |word|
-    f_output.puts(word)
+      word = word.downcase
+        if words_hash.include?(word)
+          words_hash[word] += 1
+        else
+          words_hash[word] = 1
+        end
     end
     
 end
-
 f.close
-f_output.close
+
+hash = words_hash.sort{|a,b| b[1]<=>a[1]}
+hash.each do |key, value|
+  puts "#{key}---#{value}"
+end
